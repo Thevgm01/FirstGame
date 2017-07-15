02802a8891785a67152e4c3a7bb1240695ed2e35
Steven Dunn <thevgm01@gmail.com>
2017-07-15 17:54:33 -0600

    Initial commit

---
+++ app/src/main/java/com/stevendunn/first/MainActivity.java
@@ -0,0 +1,39 @@
+package com.stevendunn.first;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Steven Dunn on 7/15/2017.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    private GameView myGameView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        myGameView = new GameView(this);
+        setContentView(myGameView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // The following call pauses the rendering thread.
+        // If your OpenGL application is memory intensive,
+        // you should consider de-allocating objects that
+        // consume significant memory here.
+        myGameView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // The following call resumes a paused rendering thread.
+        // If you de-allocated graphic objects for onPause()
+        // this is a good place to re-allocate them.
+        myGameView.onResume();
+    }
+
+}
